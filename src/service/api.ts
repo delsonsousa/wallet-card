827dd1c8df3460b63bd43ba0792b1d60be61bb62
Delson Sousa <delsonsousa@MacBook-Pro-de-Delson.local>
2024-01-10 18:00:36 -0300

    change api properties

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { AppError } from "../utils/AppError";
 
+const YOUR_IP = "localhost";
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: `http://${YOUR_IP}:3333`,
 });
 
 api.interceptors.response.use(
